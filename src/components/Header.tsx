ec778d9b706bb51b4169afb7f11ab74f321c3a69
hungphongtruong <programmingd32@gmail.com>
2021-10-29 19:13:55 +0700

    init

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import {Box} from "@mui/material";
+import Image from 'next/image';
+import logo from '../assets/logo.png'
 
 export default function Header(): JSX.Element {
     return <Box
@@ -8,6 +10,15 @@ export default function Header(): JSX.Element {
             width: '100%',
             height: theme => theme.variables.appBarHeight,
             top: 0,
-            gridTemplateColumns: '1fr auto 1fr'
-        }}></Box>
+            gridTemplateColumns: '1fr auto 1fr',
+            alignItems: "center",
+            background: 'white',
+            boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.11)'
+        }}>
+        <Box sx={{gridColumn: 1}}></Box>
+        <Box sx={{gridColumn: 2, width: '28.8vw', height: theme => theme.variables.appBarHeight}}>
+            <Image src={logo} alt={'logo'} objectFit={"contain"}/>
+        </Box>
+        <Box sx={{gridColumn: 3}}></Box>
+    </Box>
 }

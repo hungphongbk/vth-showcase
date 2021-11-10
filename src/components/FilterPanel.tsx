ecb30f2e8b79d6dee21e8e6102c96e5efab3d3db
hungphongtruong <programmingd32@gmail.com>
2021-11-10 20:23:29 +0700

    add toggle

--- src/components/FilterPanel.tsx
+++ src/components/FilterPanel.tsx
@@ -1,8 +1,9 @@
 import React, { PropsWithChildren, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import { MotionBox } from "./commons";
 import { SxProps } from "@mui/system";
+import Switch from "./Switch";
 
 const FilterTag = ({
   sx,
@@ -26,7 +27,8 @@ const FilterTag = ({
 );
 
 export default function FilterPanel(): JSX.Element {
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState(""),
+    [s2, setS2] = useState<Record<string, boolean | undefined>>({});
   return (
     <MotionBox
       initial={{ y: "100%", opacity: 0 }}
@@ -61,7 +63,7 @@ export default function FilterPanel(): JSX.Element {
           <RefreshIcon sx={{ color: "white" }} />
         </Box>
       </Box>
-      <Box sx={{ display: "flex", gap: 1, mt: 1 }}>
+      <Box sx={{ display: "flex", gap: 1, mt: 1, mb: 1.5 }}>
         {["Nhiều nhất/Tuần", "Nhiều nhất/Tháng"].map((content) => (
           <FilterTag
             key={content}
@@ -74,6 +76,69 @@ export default function FilterPanel(): JSX.Element {
             {content}
           </FilterTag>
         ))}
+      </Box>
+      <Divider />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          my: 1.5,
+        }}
+      >
+        <Typography sx={{ fontWeight: 600 }}>
+          Thời gian bán dự kiến gần nhất
+        </Typography>
+        <Switch defaultChecked />
+      </Box>
+      <Divider />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          my: 1.5,
+        }}
+      >
+        <Typography sx={{ fontWeight: 600 }}>
+          Thời gian cập nhật mới nhất
+        </Typography>
+        <Switch />
+      </Box>
+      <Divider />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          my: 2,
+        }}
+      >
+        <Typography sx={{ fontWeight: 600 }}>Giá bán trước tốt nhất</Typography>
+        <Switch />
+      </Box>
+      <Divider />
+      <Box>
+        <Typography sx={{ fontWeight: 600, my: 1.5 }}>Được đánh giá</Typography>
+        <Box sx={{ display: "flex", gap: 1, mt: 1, mb: 1.5 }}>
+          {["Siêu phẩm", "Có tiềm năng", "Đáng tiền"].map((content) => (
+            <FilterTag
+              key={content}
+              sx={{
+                bgcolor: s2[content] === true ? "#FFDE50" : "#f1f1f1",
+                fontWeight: s2[content] === true ? 600 : 500,
+              }}
+              onClick={() =>
+                setS2({
+                  ...s2,
+                  [content]: !s2[content],
+                })
+              }
+            >
+              {content}
+            </FilterTag>
+          ))}
+        </Box>
       </Box>
     </MotionBox>
   );
---
+++ src/components/Switch.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import MuiSwitch, { SwitchProps } from "@mui/material/Switch";
+
+const HEIGHT = 18,
+  WIDTH = 39;
+
+const Switch = styled((props: SwitchProps) => (
+  <MuiSwitch
+    focusVisibleClassName=".Mui-focusVisible"
+    disableRipple
+    {...props}
+  />
+))(({ theme }) => ({
+  width: WIDTH,
+  height: HEIGHT,
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
+    padding: 0,
+    margin: 2,
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: `translateX(${WIDTH - HEIGHT}px)`,
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: "#FFDE50",
+        opacity: 1,
+        border: 0,
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: 0.5,
+      },
+    },
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      color: "#FFDE50",
+      border: "6px solid #fff",
+    },
+    "&.Mui-disabled .MuiSwitch-thumb": {
+      color:
+        theme.palette.mode === "light"
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: HEIGHT - 4,
+    height: HEIGHT - 4,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: HEIGHT / 2,
+    backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
+    opacity: 1,
+    transition: theme.transitions.create(["background-color"], {
+      duration: 500,
+    }),
+  },
+}));
+
+export default Switch;

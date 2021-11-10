ed5037a41ea571dacf94d96f25f5380e70e53ff2
hungphongtruong <programmingd32@gmail.com>
2021-11-10 19:13:18 +0700

    add toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
 import type { InferGetStaticPropsType } from "next";
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import demoData from "../src/assets/data";
 import ProductList from "../src/components/ProductList";
 import { motion } from "framer-motion";
+import TuneIcon from "@mui/icons-material/Tune";
+import { PropsWithChildren } from "react";
+import { SxProps } from "@mui/system";
 
 const MotionContainer = motion(Container);
+
+const FilterTag = (props: PropsWithChildren<{ sx?: SxProps }>) => (
+  <Box
+    sx={{
+      height: 27,
+      px: 2,
+      borderRadius: "13.5px",
+      display: "flex",
+      alignItems: "center",
+      fontWeight: 600,
+      ...props.sx,
+    }}
+  >
+    {props.children}
+  </Box>
+);
 
 function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
@@ -14,7 +33,34 @@ function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
       animate={{ opacity: 1 }}
       exit={{}}
     >
-      <ProductList posts={posts} />
+      <ProductList posts={posts.slice(0, 2)} variant={"standard"} />
+      <Box sx={{ my: 1, maxWidth: "100%", overflowX: "scroll" }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+          <Box
+            sx={{
+              width: 28,
+              height: 28,
+              borderRadius: "50%",
+              bgcolor: "#FFDE50",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TuneIcon sx={{ width: 16, height: 16 }} />
+          </Box>
+          <FilterTag sx={{ bgcolor: "#d5d5d5", color: "white" }}>
+            Coming Soon
+          </FilterTag>
+          <FilterTag sx={{ bgcolor: "#FFDE50", color: "black" }}>
+            Showcase
+          </FilterTag>
+          <FilterTag sx={{ bgcolor: "#d5d5d5", color: "white" }}>
+            Idea
+          </FilterTag>
+        </Box>
+      </Box>
+      <ProductList posts={posts.slice(2)} />
     </MotionContainer>
   );
 }

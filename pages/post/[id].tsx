1bcb1172e9fa551a0392403980c0ddb25021bdb8
hungphongtruong <programmingd32@gmail.com>
2021-11-11 10:16:09 +0700

    add toggle

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,9 +1,10 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import demoData from "../../src/assets/data";
-import { Box } from "@mui/material";
+import { Box, Button, IconButton } from "@mui/material";
 import ProductDetailed from "../../src/components/ProductDetailed";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function PostDetailedPage({
   post,
@@ -21,6 +22,59 @@ export default function PostDetailedPage({
         onClick={() => router.push("/")}
         posts={posts}
       />
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          display: "grid",
+          gridTemplateColumns: "1fr 0 1fr",
+          p: 1,
+          height: 60,
+          zIndex: 12,
+          bgcolor: "white",
+          boxShadow: "0px -4px 30px rgba(0, 0, 0, 0.15)",
+        }}
+      >
+        <Button
+          variant={"contained"}
+          sx={{
+            bgcolor: "#F3F3F3",
+            color: "black",
+            fontWeight: 600,
+            fontSize: 15,
+            boxShadow: "none",
+          }}
+        >
+          Hợp tác
+        </Button>
+        <IconButton
+          sx={{
+            width: 66,
+            height: 66,
+            bgcolor: "#FFDE50",
+            border: "8px solid white",
+            ml: "-33px",
+            mt: "-22px",
+            zIndex: 2,
+          }}
+        >
+          <ShoppingCartIcon sx={{ height: 30, width: 30 }} />
+        </IconButton>
+        <Button
+          variant={"contained"}
+          sx={{
+            bgcolor: "#F3F3F3",
+            color: "black",
+            fontWeight: 600,
+            fontSize: 15,
+            boxShadow: "none",
+          }}
+        >
+          Lưu dự án
+        </Button>
+      </Box>
     </Box>
   );
 }

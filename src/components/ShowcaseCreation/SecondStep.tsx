6ddeeb76dcb3c15fba2b68c6fd5cb7e993fa2827
hungphongtruong <programmingd32@gmail.com>
2021-11-25 00:47:49 +0700

    init second step

--- src/components/ShowcaseCreation/SecondStep.tsx
+++ src/components/ShowcaseCreation/SecondStep.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import { useShowcaseCreation } from "../../layout/ShowcaseCreationLayout";
+import { useRouter } from "next/router";
 
 export default function SecondStep(): JSX.Element {
   const { showcase, dispatch } = useShowcaseCreation();
-  console.log(showcase);
+  const router = useRouter();
+
+  /**
+   * If status not set, must go back to step 1
+   * just convenience for developmemt process
+   */
+  useEffect(() => {
+    if (!showcase.status) {
+      // noinspection JSIgnoredPromiseFromCall
+      router.replace("/manage/create-post/step1");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return <Box />;
 }

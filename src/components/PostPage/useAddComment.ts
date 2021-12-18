8983f60b6190fe54aaef26b874f5632a07f967f9
hungphongtruong <programmingd32@gmail.com>
2021-12-18 15:44:04 +0700

    refactor apollo

--- src/components/PostPage/useAddComment.ts
+++ src/components/PostPage/useAddComment.ts
@@ -1,28 +1,21 @@
-import { useMutation, useQuery } from "@apollo/client";
 import {
   CommentCreateDto,
-  PostAuthorizedCommentMutation,
-  PostAuthorizedCommentMutationVariables,
-  QueryCommentsQuery,
-  QueryCommentsQueryVariables,
+  refetchQueryCommentsQuery,
+  usePostAuthorizedCommentMutation,
+  useQueryCommentsQuery,
 } from "../../types/graphql";
-import { postAuthorizedComment, queryComments } from "../../api";
 import { useCallback } from "react";
 
 type UseAddCommentProps = {
   slug: string;
 };
 export const useAddComment = ({ slug }: UseAddCommentProps) => {
-  const { data, loading } = useQuery<
-    QueryCommentsQuery,
-    QueryCommentsQueryVariables
-  >(queryComments, { variables: { slug } });
-  const [mutate, { loading: adding, error }] = useMutation<
-    PostAuthorizedCommentMutation,
-    PostAuthorizedCommentMutationVariables
-  >(postAuthorizedComment, {
-    refetchQueries: [{ query: queryComments, variables: { slug } }],
-  });
+  const { data, loading } = useQueryCommentsQuery({ variables: { slug } });
+  const [mutate, { loading: adding, error }] = usePostAuthorizedCommentMutation(
+    {
+      refetchQueries: [refetchQueryCommentsQuery({ slug })],
+    }
+  );
 
   const addComment = useCallback(
     (input: CommentCreateDto) => mutate({ variables: { slug, input } }),

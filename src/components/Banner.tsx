8ce8ee069f48bdbae5604a5d9aa8e45663812087
hungphongtruong <programmingd32@gmail.com>
2021-12-22 07:03:23 +0700

    remove unused codes

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Typography } from "@mui/material";
 import { SxProps } from "@mui/system";
 import { MediaDto, QueryBannerQuery } from "../types/graphql";
-import { wrap } from "popmotion";
 import { AspectRatio } from "@hungphongbk/vth-sdk";
 import { sxFlexCenter, sxFullSize } from "../utils/predefinedSx";
 import SlickSlider from "./SlickSlider";
@@ -13,13 +12,7 @@ type BannerProps = {
   banner: QueryBannerQuery["banner"];
 };
 export default function Banner({ sx, banner }: BannerProps): JSX.Element {
-  const [[page, direction], setPage] = useState([0, 0]),
-    bannerCount = banner?.value?.images?.length ?? 0;
-
-  const goBack = () => setPage([page - 1, -1]),
-    goNext = () => setPage([page + 1, 1]);
-
-  const imageIndex = wrap(0, bannerCount, page);
+  const bannerCount = banner?.value?.images?.length ?? 0;
   return (
     <>
       {bannerCount === 0 ? (

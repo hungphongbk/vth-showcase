c85fbea9bf043ad5d822cea0de50f9d9348a74ba
hungphongtruong <programmingd32@gmail.com>
2021-12-24 11:34:47 +0700

    complete initial script

--- sdk/src/App.tsx
+++ sdk/src/App.tsx
@@ -1,5 +1,23 @@
+import { useShowcasePortalQuery } from "../../src/types/graphql";
+import { StyledSection, StyledTitle } from "./styled";
 import { Typography } from "@mui/material";
 
 export default function App() {
-  return <Typography>Testing</Typography>;
+  const { data } = useShowcasePortalQuery();
+  return (
+    <StyledSection>
+      <div className="container">
+        <StyledTitle className="vth-section-title">
+          <span>SHOWCASE</span>
+        </StyledTitle>
+        <div>
+          {data?.showcases.edges.map(({ node }) => (
+            <Typography key={node.slug} variant={"h6"}>
+              {node.name}
+            </Typography>
+          ))}
+        </div>
+      </div>
+    </StyledSection>
+  );
 }
---
+++ sdk/src/ShowcaseItem.tsx
@@ -0,0 +1,4 @@
+type ShowcaseItemProps = {};
+export default function ShowcaseItem(props: ShowcaseItemProps): JSX.Element {
+  return <div></div>;
+}

6cc5625c56a224e11cc8adcae308f3e396b8dc79
hungphongtruong <programmingd32@gmail.com>
2022-01-01 14:41:28 +0700

    integrate featured products

--- next.config.js
+++ next.config.js
@@ -2,46 +2,51 @@ const withTM = require("next-transpile-modules")([
   "@mui/material",
   "@mui/system",
 ]); // pass the modules you would like to see transpiled
-
-module.exports = withTM({
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@mui/styled-engine": "@mui/styled-engine-sc",
-    };
-    config.module.rules.push({
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: "graphql-tag/loader",
-    });
-    return config;
-  },
-  images: {
-    domains: [
-      "localhost",
-      "showcase-dev.vaithuhay.com",
-      "api.showcase-dev.vaithuhay.com",
-      "media.showcase-dev.vaithuhay.com",
-      "showcase.vaithuhay.com",
-      "api.showcase.vaithuhay.com",
-      "i.imgur.com",
-      "product.hstatic.net",
-      ...(process.env.NODE_ENV === "development" ? ["207.148.126.213"] : []),
-    ],
-  },
-  experimental: {
-    scrollRestoration: true,
-    styledComponent: true,
-  },
-  staticPageGenerationTimeout: 300,
-  async redirects() {
-    return [
-      {
-        source: "/manage/create-post",
-        destination: "/manage/create-post/step1",
-        permanent: true,
-      },
-    ];
-  },
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+module.exports = withBundleAnalyzer(
+  withTM({
+    swcMinify: true,
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@mui/styled-engine": "@mui/styled-engine-sc",
+      };
+      config.module.rules.push({
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: "graphql-tag/loader",
+      });
+      return config;
+    },
+    images: {
+      domains: [
+        "localhost",
+        "showcase-dev.vaithuhay.com",
+        "api.showcase-dev.vaithuhay.com",
+        "media.showcase-dev.vaithuhay.com",
+        "showcase.vaithuhay.com",
+        "api.showcase.vaithuhay.com",
+        "i.imgur.com",
+        "product.hstatic.net",
+        ...(process.env.NODE_ENV === "development" ? ["207.148.126.213"] : []),
+      ],
+    },
+    experimental: {
+      scrollRestoration: true,
+      styledComponent: true,
+    },
+    staticPageGenerationTimeout: 300,
+    async redirects() {
+      return [
+        {
+          source: "/manage/create-post",
+          destination: "/manage/create-post/step1",
+          permanent: true,
+        },
+      ];
+    },
+  })
+);

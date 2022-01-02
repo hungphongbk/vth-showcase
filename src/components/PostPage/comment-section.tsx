e957b398c5901864a9dfbbf2457fd812bde8269a
hungphongtruong <programmingd32@gmail.com>
2022-01-02 15:49:49 +0700

    enhance comment

--- src/components/PostPage/comment-section.tsx
+++ src/components/PostPage/comment-section.tsx
@@ -2,12 +2,26 @@ import CollapseCard from "../CollapseCard";
 import CommentWrite from "./comment-write";
 import { useAddComment } from "./useAddComment";
 import { Avatar, Box, Stack, styled, Typography } from "@mui/material";
+import { CommentRateMaps } from "./styled";
 
 const CommentContent = styled(Box)`
   /* Neutral/Dark White */
   background: #f3f3f3;
   border-radius: 10px;
   padding: 11px 15px;
+`;
+
+const CommentChip = styled(Box)`
+  height: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 9px;
+  padding-left: 9px;
+  padding-right: 9px;
+  color: white;
+  font-size: 10px;
+  line-height: 12px;
 `;
 
 type CommentSectionProps = { slug: string };
@@ -41,10 +55,23 @@ export default function CommentSection(
                       typeof comment.author?.name === "undefined"
                         ? "italic"
                         : "normal",
+                    mb: 0.3,
                   }}
                 >
                   {comment.author?.name ?? "Người dùng"}
                 </Typography>
+                {comment.rate.length > 0 && (
+                  <Stack gap={1} direction={"row"} sx={{ mb: 0.4 }}>
+                    {comment.rate.map((rate) => {
+                      const map = CommentRateMaps[rate];
+                      return (
+                        <CommentChip sx={{ bgcolor: map.color }} key={rate}>
+                          {map.label}
+                        </CommentChip>
+                      );
+                    })}
+                  </Stack>
+                )}
                 <Typography>{comment.content}</Typography>
               </CommentContent>
             </Box>

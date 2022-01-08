6c7a70b20c5ec38a5b9ad3df1e396760e47ad2e8
hungphongtruong <programmingd32@gmail.com>
2022-01-08 16:02:47 +0700

    update sdk

--- src/components/system/preorder-button.tsx
+++ src/components/system/preorder-button.tsx
@@ -1,7 +1,11 @@
 import { useAuthInitialized } from "../../utils/hooks";
-import { PreorderFilledPrimaryIcon } from "@hungphongbk/vth-sdk";
+import {
+  CheckFilledPrimaryIcon,
+  PreorderFilledPrimaryIcon,
+} from "@hungphongbk/vth-sdk";
 import VthIconButton from "../vth-icon-button";
 import { Showcase } from "../../types/graphql";
+import { useEffect, useMemo, useState } from "react";
 
 type PreorderButtonProps = {
   showcase: Pick<Showcase, "status" | "slug" | "expectedSalePrice">;
@@ -9,12 +13,25 @@ type PreorderButtonProps = {
 export default function PreorderButton(
   props: PreorderButtonProps
 ): JSX.Element {
-  const { initialized, isLoggedIn } = useAuthInitialized();
+  const { initialized, isLoggedIn } = useAuthInitialized(),
+    [open, setOpen] = useState(false),
+    [isSubmitting, setIsSubmitting] = useState(false),
+    [isSubmitted, setIsSubmitted] = useState(false);
+
+  const IconComponent = useMemo(() => {
+    if (isSubmitted) return CheckFilledPrimaryIcon;
+    return PreorderFilledPrimaryIcon;
+  }, [isSubmitted]);
+
+  useEffect(() => {
+    if (isSubmitted) setTimeout(() => setIsSubmitted(false), 1300);
+  }, [isSubmitted]);
+
   return (
     <>
       <VthIconButton
         sx={{ flexGrow: 1, opacity: initialized ? 1 : 0.5 }}
-        startIcon={<PreorderFilledPrimaryIcon sx={{ width: 22, height: 22 }} />}
+        startIcon={<IconComponent sx={{ width: 22, height: 22 }} />}
         fullWidth
         onClick={() => {
           //

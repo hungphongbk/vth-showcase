768e40999d97d57391b8c98e81d46723a2644a89
hungphongtruong <programmingd32@gmail.com>
2022-01-08 16:07:53 +0700

    remove graphql file

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,8 +3,6 @@ import * as apiService from "../service/graphql.service";
 
 export { apiService };
 
-export { default as mutationCreateMedia } from "./createMedia.graphql";
-export { default as mutationDeleteMedia } from "./deleteMedia.graphql";
 export { default as mutationCreateShowcase } from "./createShowcase.graphql";
 
 export { default as postAnonymousComment } from "./postAnonymousComment.graphql";

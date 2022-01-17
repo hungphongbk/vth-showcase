8f679fdbd57b4740e7dd8cc365e986c3fe887a32
hungphongtruong <programmingd32@gmail.com>
2022-01-17 11:00:55 +0700

    remove cart redux

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,5 +1,4 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import cartReducer from "./cart/reducer";
 import { authReducer } from "./auth.reducer";
 
-export default combineReducers({ cart: cartReducer, auth: authReducer });
+export default combineReducers({ auth: authReducer });

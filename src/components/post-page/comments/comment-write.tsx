f66f61fa391808d7fc402c5614126eafdda4e683
hungphongtruong <programmingd32@gmail.com>
2022-01-18 14:07:12 +0700

    change SDK

--- src/components/post-page/comments/comment-write.tsx
+++ src/components/post-page/comments/comment-write.tsx
@@ -1,17 +1,8 @@
-import {
-  Box,
-  Button,
-  css,
-  Stack,
-  styled,
-  ToggleButton,
-  ToggleButtonGroup,
-} from "@mui/material";
+import { Box, Button, Stack, styled } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { CommentCreateDto } from "../../../types/graphql";
-import { FormInput } from "@hungphongbk/vth-sdk";
+import { CommentRateSelector, FormInput } from "@hungphongbk/vth-sdk";
 import React from "react";
-import { CommentRateMaps } from "../styled";
 
 const CommentBox = styled(Box)`
     /* Neutral/Dark White */
@@ -30,39 +21,6 @@ const CommentBox = styled(Box)`
     background-color: unset;
     padding: 8px;
   `;
-const StyledToggleButtonGroup = styled(ToggleButtonGroup)(
-  ({ theme }) => css`
-    .MuiToggleButtonGroup-grouped {
-      margin: ${theme.spacing(0.5)};
-      border: 0;
-
-      &.Mui-selected {
-        color: white;
-      }
-      text-transform: capitalize;
-      height: 20px;
-      padding: 0 10px;
-
-      &:not(:first-of-type) {
-        border-radius: 10px;
-      }
-      &:first-of-type {
-        border-radius: 10px;
-      }
-    }
-  `
-);
-const StyledToggleButton = styled(ToggleButton)<{ selectedColor: string }>(
-  ({ selectedColor }) => css`
-    font-size: 11px;
-    &.Mui-selected {
-      &,
-      &:hover {
-        background-color: ${selectedColor};
-      }
-    }
-  `
-);
 
 type CommentWriteProps = {
   onSubmit: (values: CommentCreateDto) => Promise<any>;
@@ -95,15 +53,9 @@ export default function CommentWrite(props: CommentWriteProps): JSX.Element {
         <FormInput
           name={"rate"}
           control={control}
-          component={StyledToggleButtonGroup}
+          component={CommentRateSelector}
           sx={{ p: 0.5 }}
-        >
-          {Object.entries(CommentRateMaps).map(([_enum, { label, color }]) => (
-            <StyledToggleButton key={_enum} selectedColor={color} value={_enum}>
-              {label}
-            </StyledToggleButton>
-          ))}
-        </FormInput>
+        />
       </CommentBox>
       <Button
         variant={"contained"}

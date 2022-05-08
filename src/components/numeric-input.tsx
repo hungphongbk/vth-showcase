8c783f15602eb4be07c3325b55294259d553866e
Phong Truong Hung <104372894+asombredance1218@users.noreply.github.com>
2022-05-08 17:21:32 +0700

    continue on numeric input

--- src/components/numeric-input.tsx
+++ src/components/numeric-input.tsx
@@ -1,9 +1,12 @@
 import {
+  ChangeEvent,
   ChangeEventHandler,
+  KeyboardEventHandler,
   MouseEventHandler,
   ReactNode,
   useCallback,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from "react";
@@ -11,35 +14,70 @@ import styles from "./numeric-input.module.css";
 import { Box } from "@mui/system";
 import { range } from "lodash";
 
-type NumericInputProps = {
+export type NumericInputProps = {
   value: number;
   onChange: ChangeEventHandler;
   numOfMask?: number;
   suffix?: ReactNode;
+  name?: string;
 };
 export default function NumericInput({
   value,
   onChange,
   numOfMask = 2,
   suffix,
+  name,
 }: NumericInputProps): JSX.Element {
   const [tempVal, setTempVal] = useState(() => value),
     [pos, setPos] = useState(0),
-    rootRef = useRef<HTMLDivElement>();
+    rootRef = useRef<HTMLDivElement>(),
+    tempValStr = useMemo(
+      () => (tempVal === 0 ? [""] : (tempVal + "").split("")),
+      [tempVal]
+    );
 
   useEffect(() => {
     setTempVal(value);
   }, [value]);
 
+  useEffect(() => {
+    if (pos === numOfMask) {
+      const event = new Event("change") as unknown as ChangeEvent;
+      Object.defineProperty(event, "target", {
+        writable: true,
+        value: {
+          value: tempVal,
+          name,
+        },
+      });
+      onChange(event);
+    }
+  }, [name, numOfMask, onChange, pos, tempVal]);
+
   const onFocus = useCallback<MouseEventHandler>(() => {
     rootRef.current?.focus();
   }, []);
+
+  const onKeyDown = useCallback<KeyboardEventHandler>(
+    (e) => {
+      if (/\d/.test(e.key) && pos < numOfMask) {
+        const newVal = tempVal * 10 + parseInt(e.key);
+        setTempVal(newVal);
+        setPos(pos + 1);
+      } else if (e.key === "Backspace") {
+        setTempVal(Math.round(tempVal / 10));
+        setPos(pos - 1);
+      }
+    },
+    [numOfMask, pos, tempVal]
+  );
 
   return (
     <Box
       ref={rootRef}
       tabIndex={-1}
       onClick={onFocus}
+      onKeyDown={onKeyDown}
       className={styles.Root}
       sx={{
         gridTemplateColumns: `${range(numOfMask)
@@ -48,8 +86,11 @@ export default function NumericInput({
       }}
     >
       {range(numOfMask).map((_, index) => (
-        <Box key={index} className={styles.Cell}></Box>
+        <Box key={index} className={styles.Cell}>
+          {tempValStr[index] ?? ""}
+        </Box>
       ))}
+      {suffix && <Box className={styles.Text}>{suffix}</Box>}
     </Box>
   );
 }
--- src/components/showcase-creation/p-l-input.tsx
+++ src/components/showcase-creation/p-l-input.tsx
@@ -2,9 +2,9 @@ import { Control, FieldPath } from "react-hook-form";
 import { ShowcaseCreateInputDto } from "../../types/graphql";
 import { Box } from "@mui/system";
 import { ReactElement } from "react";
-import { Tooltip, Typography } from "@mui/material";
+import { Stack, Tooltip, Typography } from "@mui/material";
 import styles from "./p-l-input.module.css";
-import NumericInput from "../numeric-input";
+import NumericInput, { NumericInputProps } from "../numeric-input";
 
 type PLControl = Control<ShowcaseCreateInputDto>;
 
@@ -14,13 +14,13 @@ function PLInputRow({
   label,
   tooltip,
   numOfMask = 2,
+  suffix,
 }: {
   control: PLControl;
   field: FieldPath<ShowcaseCreateInputDto>;
   label: string;
   tooltip: string | ReactElement;
-  numOfMask?: number;
-}): JSX.Element {
+} & Pick<NumericInputProps, "numOfMask" | "suffix">): JSX.Element {
   return (
     <Box className={styles.PLInputRow}>
       <Typography className="font-bold">
@@ -29,7 +29,12 @@ function PLInputRow({
           <span className={styles.QuestionMark}>?</span>
         </Tooltip>
       </Typography>
-      <NumericInput value={0} onChange={() => {}} numOfMask={numOfMask} />
+      <NumericInput
+        value={0}
+        onChange={() => {}}
+        numOfMask={numOfMask}
+        suffix={suffix}
+      />
     </Box>
   );
 }
@@ -39,7 +44,7 @@ type PLInputProps = {
 };
 export default function PLInput(props: PLInputProps): JSX.Element {
   return (
-    <>
+    <Stack gap={2}>
       <PLInputRow
         control={props.control}
         field={"inventory.capitalizationRate"}
@@ -47,7 +52,26 @@ export default function PLInput(props: PLInputProps): JSX.Element {
         tooltip={
           "Giá cogs nghĩa là giá đã bao gồm chi phí vận chuyển, nguyên liệu, giá nhập, giá gia công, bao bì đóng gói, tem nhãn giao đến kho Vaithuhay."
         }
+        suffix={"%"}
       />
-    </>
+      <PLInputRow
+        control={props.control}
+        field={"inventory.adCostRate"}
+        label={"Tỉ lệ chi phí quảng cáo trung bình (Gồm giảm giá)"}
+        tooltip={
+          "Chi phí chạy quảng cáo, thương hiệu, booking báo chí, KOLs, chiến lược quảng cáo, đầu tư ban đầu (website, sàn, nhận diện thương hiệu....) "
+        }
+        suffix={"%"}
+      />
+      <PLInputRow
+        control={props.control}
+        field={"inventory.operatingCostRate"}
+        label={"Tỉ lệ chi phí vận hành"}
+        tooltip={
+          "Là chi phí nhân sự bán hàng, đóng gói hàng hóa, nhân viên quảng cáo, nhân sự quản lý được Vaithuhay chi trả, kế toán hạch toán dự án...."
+        }
+        suffix={"%"}
+      />
+    </Stack>
   );
 }
